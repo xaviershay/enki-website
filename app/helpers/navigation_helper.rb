6469bd458abb73ee7ae5a912b0c52b3f1ea7c08a
Xavier Shay <xavier@rhnh.net>
2008-01-19 17:39:27 +1100

    Pages navigation

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,9 +1,7 @@
 module NavigationHelper
   def page_links_for_navigation
     link = Struct.new(:name, :url)
-    [link.new("Home", posts_path),
-     link.new("Archives", archives_path)] + 
-      Page.find(:all, :order => 'title').collect {|page| link.new(page.title, page_path(page))}
+    Page.find(:all, :order => 'title').collect {|page| link.new(page.title, page_path(page))}
   end
 
   def category_links_for_navigation
