de8b211d828e9105fd08312eb3fe079391e5fa3b
Xavier Shay <xavier@rhnh.net>
2013-01-22 22:01:17 -0800

    Use new secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Enki::Application.config.secret_token = '1481948abe0b49729e01200f00033b021cf2e7e1128d37da0cb637c1b661f99cf3bc163c5ba57f164c02254d9255591358c502ddeb1107fe2647af3c5bfa0c37'
+Enki::Application.config.secret_token = ENV['SECRET_TOKEN'] ||
+                                        File.read("config/secret_token")
